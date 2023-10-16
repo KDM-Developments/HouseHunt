e0a85f3d4f532de549a1d63ff3bfed718a73fbf4
constGumo <damianmichniewskigd@gmail.com>
2023-10-16 20:44:34 +0200

    Cleaning files

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import mainhome from "./content/homepage/mainhome.svg";
+import stars1 from "./content/homepage/stars-home.svg";
+import stars2 from "./content/homepage/stars-topic.svg";
+import logo from "./content/homepage/househuntlogo-small.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="App-header background2">
+        <img src={logo} className="logo-small" />
+        <img src={stars1} className="stars1" />
+        <img src={mainhome} alt="Home" className="home" />
+        {/* <img src={background} className="background" alt="background" /> */}
+        <span className="intro-text">
+          Start Your House Hunt here!
+          <img src={stars2} className="stars2" />
+        </span>
+        <p className="intro-description">
+          Browes and add appartment’s listing for free
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button className="sign-in">Sign in</button>
+        <button className="log-in">Log in</button>
+      </div>
     </div>
   );
 }
